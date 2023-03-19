6bb6f38a0922a4b0dee27054bbe3da33af1c4cc4
Vu Hoang Giang <katana2812@gmail.com>
2023-03-19 11:02:27 +0700

    add binarySearch

--- app_root/backend/tests/my_first_test.go
+++ app_root/backend/tests/my_first_test.go
@@ -11,6 +11,24 @@ func Calculate(x int) (result int) {
 	return result
 }
 
+func binarySearch(arr []int, x int) int {
+	low := 0
+	high := len(arr) - 1
+
+	for low <= high {
+			mid := (low + high) / 2
+			if arr[mid] < x {
+					low = mid + 1
+			} else if arr[mid] > x {
+					high = mid - 1
+			} else {
+					return mid
+			}
+	}
+
+	return -1
+}
+
 func TestCalculate(t *testing.T) {
 	assert := assert.New(t)
 
@@ -56,4 +74,20 @@ func TestSomethingMore(t *testing.T) {
 	// Assert that a slice contains a specific element
 	slice := []int{1, 2, 3, 4}
 	assert.Contains(t, slice, 3, "slice should contain 3")
+}
+
+func TestBinarySearch(t *testing.T) {
+	arr := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	
+	// Test a value in the array
+	index := binarySearch(arr, 5)
+	assert.Equal(t, 4, index, "5 should be at index 4")
+	
+	// Test a value not in the array
+	index = binarySearch(arr, 20)
+	assert.Equal(t, -1, index, "20 should not be found in the array")
+	
+	// Test an empty array
+	index = binarySearch([]int{}, 5)
+	assert.Equal(t, -1, index, "5 should not be found in an empty array")
 }
