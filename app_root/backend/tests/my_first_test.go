71e23623c187ab05a47567cfec371ea9ebd3bb71
Vu Hoang Giang <katana2812@gmail.com>
2023-03-19 11:00:34 +0700

    add simple unit test

--- app_root/backend/tests/my_first_test.go
+++ app_root/backend/tests/my_first_test.go
@@ -42,4 +42,18 @@ func TestSomethingMore(t *testing.T) {
 
   // assert inequality
   assert.NotEqual(t, 123, 456, "they should not be equal")
+
+	// Assert that two strings are equal
+	assert.Equal(t, "Hello, world!", "Hello, world!", "The two strings should be equal")
+
+	// Assert that a condition is true
+	assert.True(t, 1+1 == 2, "1+1 should equal 2")
+
+	// Assert that a value is nil
+	var x *int
+	assert.Nil(t, x, "x should be nil")
+
+	// Assert that a slice contains a specific element
+	slice := []int{1, 2, 3, 4}
+	assert.Contains(t, slice, 3, "slice should contain 3")
 }
