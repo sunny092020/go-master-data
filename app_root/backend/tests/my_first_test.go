930101c88c057c6d95ddce81b6d2f1a2a55ade3d
Vu Hoang Giang <katana2812@gmail.com>
2023-03-19 10:49:43 +0700

    add simple unit test

---
+++ app_root/backend/tests/my_first_test.go
@@ -0,0 +1,35 @@
+package my_first_test
+
+import (
+	"testing"
+	"github.com/stretchr/testify/assert"
+)
+
+// Calculate returns x + 2.
+func Calculate(x int) (result int) {
+	result = x + 2
+	return result
+}
+
+func TestCalculate(t *testing.T) {
+	assert := assert.New(t)
+
+	var tests = []struct {
+			input    int
+			expected int
+	}{
+			{2, 4},
+			{-1, 1},
+			{0, 2},
+			{-5, -3},
+			{99999, 100001},
+	}
+
+	for _, test := range tests {
+			assert.Equal(Calculate(test.input), test.expected)
+	}
+}
+
+func TestSomething(t *testing.T) {
+  assert.True(t, true, "True is true!")
+}
---
+++ app_root/scripts/test.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker exec -it md_backend go test tests/my_first_test.go
